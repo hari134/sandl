d897e87e68e200ebe99c028c507e14e77c02a265
hari vishwanath <mailtohari134@gmail.com>
2023-12-31 20:08:04 +0530

    .

--- _parser.py
+++ _parser.py
@@ -1,6 +1,6 @@
 import ply.lex as lex
 import ply.yacc as yacc
-from prettytable import PrettyTable
+import json
 
 # Define tokens
 tokens = (
@@ -31,68 +31,119 @@ def t_error(t):
 # Build the lexer
 lexer = lex.lex()
 
-# Initialize an empty stack
-stack = []
-
-# Create a table for the parsing steps
-table = PrettyTable(['Step', 'Stack', 'Operation'])
-
 # Define the CFG rules with valid ranges and checks
 def p_game(p):
     '''game : statements'''
-    print("Reducing: <statements> -> <game>")
-    print("Game parsed successfully!")
-    p[0] = p[1]
 
 def p_statements(p):
     '''statements : statement
                   | statements statement'''
-    p[0] = f"{p[1]} {p[2]}" if len(p) == 3 else p[1]
-    print(f"Reducing: <statements> -> {p[0]}")
-    stack.append(p[0])
-    table.add_row([len(stack), stack.copy(), 'Reduce'])
 
 def p_statement(p):
     '''statement : PLAYER move'''
-    p[0] = f"{p[1]} {p[2]}"
-    print(f"Reducing: <statement> -> {p[0]}")
-    stack.append(p[0])
-    table.add_row([len(stack), stack.copy(), 'Reduce'])
 
 def p_move_dice(p):
     '''move : DICE_MOVE NUMBER'''
-    p[0] = f"{p[1]} {p[2]}"
-    print(f"Shifting: <move> -> {p[0]}")
-    stack.append(p[0])
-    table.add_row([len(stack), stack.copy(), 'Shift'])
 
 def p_move_snake(p):
     '''move : SNAKE_MOVE NUMBER TO NUMBER'''
-    p[0] = f"{p[1]} {p[2]} {p[3]} {p[4]}"
-    print(f"Shifting: <move> -> {p[0]}")
-    stack.append(p[0])
-    table.add_row([len(stack), stack.copy(), 'Shift'])
 
 def p_move_ladder(p):
     '''move : LADDER_MOVE NUMBER TO NUMBER'''
-    p[0] = f"{p[1]} {p[2]} {p[3]} {p[4]}"
-    print(f"Shifting: <move> -> {p[0]}")
-    stack.append(p[0])
-    table.add_row([len(stack), stack.copy(), 'Shift'])
 
 # Build the parser
-parser = yacc.yacc()
+parser = yacc.yacc(method='SLR')
 
 # Input string to parse
-input_string = "player1 dice_move 3"
+input_string = "player1 dice_move 3 player1 snake_move 30 to 3"
 
 # Tokenize the input
 lexer.input(input_string)
 
-# Parse the stack
-print("\nParsing the stack:")
-parser.parse(input_string)
 
-# Print the table
-print("\nParsing Table:")
-print(table)
+parsing_table = {}
+terminals_goto = parser.action
+non_terminals_goto = parser.goto
+parsing_table = terminals_goto
+
+for key,value in parsing_table.items():
+    if non_terminals_goto.get(key) is not None:
+        value.update(non_terminals_goto[key])
+
+print(json.dumps(parsing_table,indent=2))
+for i in range(len(parser.productions)):
+    print(str(i) + ' ' + parser.productions[i].str)
+
+print(json.dumps(parsing_table,indent=2))
+
+stack = []
+state_stack = [0]
+current_state = 0
+isEnded = False
+
+while True:
+    # get one token from the input buffer
+    token = lexer.token()
+    token_type = '$end' if token is None else token.type
+    token_value = '$' if token is None else token.value
+    
+    if token_value != '$':
+        stack.append(token_value)
+
+    # get action for current_state and token
+    action = parser.action[state_stack[-1]].get(token_type)
+    
+    while action is not None and action < 0:
+        current_state = -action
+        print("Reducing to " + str(current_state))
+        production = parser.productions[current_state]
+
+        # Pop twice the length of the production to handle the recursion
+        for _ in range(production.len):
+            if stack:
+                stack.pop()
+                state_stack.pop()
+            else:
+                print("Error: Stack underflow during reduction.")
+                break
+
+        print(stack)
+        stack.append(production.name)
+
+        # Check if the state_stack is not empty before accessing elements
+        if state_stack:
+            # Update the state_stack using Goto table
+            new_state = parser.goto[state_stack[-1]][production.name]
+            if new_state is not None:
+                state_stack.append(new_state)
+            else:
+                print("Error: No entry in Goto table.")
+                break
+        else:
+            print("Error: State stack is empty after reduction.")
+            break
+
+        # Get the new action for the current state and token
+        action = parser.action[state_stack[-1]].get(token_type)
+        print(state_stack)
+
+    if action is not None and action > 0:
+        current_state = action
+        state_stack.append(current_state)
+        print("Shifting to " + str(current_state))
+        print(stack)
+        print(state_stack)
+    elif action == 0:
+        current_state = 0
+        print("Accept state")
+        break
+    else:
+        print("Invalid token")
+        print(current_state)
+        print(token)
+        break
+    
+    print(stack)
+
+    if isEnded:
+        break
